275b11dfd2f4b295772ae314576f210f6736c69a
Matthew Fidler <matthew.fidler@gmail.com>
2023-02-16 18:56:20 -0600

    Finish spoofing a rxode2 data structure in R

--- R/util.R
+++ R/util.R
@@ -14,8 +14,8 @@
   checkmate::assertIntegerish(i,len=1, any.missing=FALSE)
   .Call(`_rxode2parse_getWh`, as.integer(i))
 }
+
 #' This converts NONMEM-style EVIDs to classic RxODE events
-#'
 #'  
 #' @param cmt compartment flag
 #' @param amt dose amount
@@ -46,13 +46,78 @@
 .toClassicEvid <- function(cmt=1L, amt=0.0, rate=0.0, dur=0.0, ii=0.0, evid=0L, ss=0.0) {
   checkmate::assertIntegerish(cmt, any.missing=FALSE)
   checkmate::assertIntegerish(evid, any.missing=FALSE)
-  checkmate::assertNumeric(amt, any.missing=FALSE)
+  checkmate::assertNumeric(amt)
   checkmate::assertNumeric(dur, any.missing=FALSE)
-  checkmate::assertNumeric(ii, any.missing=FALSE)
-  checkmate::assertNumeric(ss, any.missing=FALSE)
-  .df <- data.frame(cmt=as.integer(cmt), evid=as.integer(evid), amt=amt,
-                    rate=rate, dur=dur, ii=ii, ss=ss)
+  checkmate::assertNumeric(ii)
+  checkmate::assertNumeric(ss)
+  .df <- data.frame(cmt=as.integer(cmt), evid=as.integer(evid), amt=as.double(amt),
+                    rate=as.double(rate), dur=as.double(dur),
+                    ii=as.double(ii),
+                    ss=as.double(ss))
   .Call(`_rxode2parse_getClassicEvid`,
         .df$cmt, .df$amt, .df$rate, .df$dur,
         .df$ii, .df$evid, .df$ss)
+}
+#' This converts data to a format similar to what rxode2 will use
+#'  
+#' @param data input dataset
+#' @return linCmt dataset
+#' @noRd
+#' @author Matthew L. Fidler
+.toLinCmtData <- function(data) {
+  .lowerNames <- tolower(names(data))
+  if ("id" %in% .lowerNames) {
+    stop("cannot solve multi-subject data, no 'id'", call.=FALSE)
+  }
+  if (!("evid" %in% .lowerNames)) {
+    stop("need 'evid'", call.=FALSE)
+  }
+  if (!("amt" %in% .lowerNames)) {
+    stop("need 'amt'", call.=FALSE)
+  }
+  if (!("time" %in% .lowerNames)) {
+    stop("need 'time'", call.=FALSE)
+  }
+  .dat <- data
+  names(.dat) <- .lowerNames
+  if (!("cmt" %in% .lowerNames)) {
+    .dat$cmt <- 1
+  }
+  .dat <- .dat[order(.dat$time),]
+  for (.v in c("rate", "dur", "ii", "ss")) {
+    if (!(.v %in% .lowerNames)) {
+      .dat[[.v]] <- 0.0
+    }
+  }
+  
+.evid <- .toClassicEvid(cmt=.dat$cmt, amt=.dat$amt,
+                          rate=.dat$rate, dur=.dat$dur,
+                          ii=.dat$ii, evid=.dat$evid,
+                          ss=.dat$ss)
+  .dat$nmEvid <- .dat$evid
+  .dat$evid <- .evid
+  .time <- .dat$time
+  .evid <- .dat$evid
+  .dat0 <- .dat[.dat$evid != 0,]
+  .datL <- do.call("rbind",
+                   lapply(seq_along(.dat0$evid),
+                          function(i) {
+                            .wh <- .getWh(.dat0$evid[i])
+                            .tinf <- 0.0
+                            if (.dat0$rate[i] > 0) {
+                              # rate = amt/time; tinf= amt/rate
+                              .tinf <- .dat0$amt[i]/.dat0$rate[i]
+                            } else if (.dat0$dur[i] > 0) {
+                              .tinf <- .dat0$dur[i]
+                            }
+                            data.frame(time=.dat0$time[i],
+                                       dose=.dat0$amt[i],
+                                       tinf=.tinf,
+                                       ii=.dat0$ii[i],
+                                       evidF=.wh["whI"],
+                                       evid0=.wh["wh0"])
+                          }))
+  row.names(.datL) <- NULL
+  list(time=.dat$time, evid=.dat$evid, linDat=.datL,
+       len=length(.datL$time))
 }
