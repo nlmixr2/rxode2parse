f6c974cee3fbc749623eec760baa9db42ae65d09
Matthew Fidler <matthew.fidler@gmail.com>
2023-02-20 21:57:08 -0600

    Fix paths and add one cmt steady state test

--- tests/testthat/test-lincmt.R
+++ tests/testthat/test-lincmt.R
@@ -1,18 +1,22 @@
-digits <- 4
-etSsB <- qs::qread("test-lincmt-etSsB.qs")
-etSsI <- qs::qread("test-lincmt-etSsI.qs")
-etSsR <- qs::qread("test-lincmt-etSsR.qs")
+tolerance <- 1e-5
+.qr <- function(x) {
+  qs::qread(testthat::test_path(x))
+}
+
+etSsB <- .qr("test-lincmt-etSsB.qs")
+etSsI <- .qr("test-lincmt-etSsI.qs")
+etSsR <- .qr("test-lincmt-etSsR.qs")
 tolerance <- 1e-5
 
 .txt <- "non sens"
 
-etSsB.ode.1c <- qs::qread("test-lincmt-etSsB.ode.1c.qs")
+etSsB.ode.1c <- .qr("test-lincmt-etSsB.ode.1c.qs")
 
 d <- etSsB.ode.1c[,c("evid", "cmt", "amt", "ii", "ss", "time")]
 
 s1 <- rxLinCmt(d, V=20, CL=25)
 
 test_that(sprintf("one compartment bolus steady state (%s)", .txt), {
-  expect_equal(etSsB.ode.1c$C2, s1$Cp, tolerance=1e-5)
+  expect_equal(etSsB.ode.1c$C2, s1$Cp, tolerance=tolerance)
 })
 
