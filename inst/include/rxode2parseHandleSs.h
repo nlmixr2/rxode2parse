072bc7ef1b1bc5511ba633cac263e40b73291f1a
Matthew L. Fidler <matthew.fidler@gmail.com>
2023-08-26 22:25:53 -0500

    Add handleSs stub

---
+++ inst/include/rxode2parseHandleSs.h
@@ -0,0 +1,16 @@
+// -*- mode: c++; c-basic-offset: 2; tab-width: 2; indent-tabs-mode: nil; -*-
+#ifndef __RXODE2PARSEHANDLSS_H___
+#define __RXODE2PARSEHANDLSS_H___
+#include "rxode2parse.h"
+#include "rxode2parseHandleEvid.h"
+#include "rxode2parseGetTime.h"
+#if defined(__cplusplus)
+extern "C" {
+#endif
+
+
+#if defined(__cplusplus)
+}
+#endif
+
+#endif
