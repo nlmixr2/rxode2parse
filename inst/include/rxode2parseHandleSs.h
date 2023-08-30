9976f0ea367e464633d1ea0867a92beb901c057f
Matthew L. Fidler <matthew.fidler@gmail.com>
2023-08-30 15:14:21 -0500

    Move things around so rxode2 will compile

--- inst/include/rxode2parseHandleSs.h
+++ inst/include/rxode2parseHandleSs.h
@@ -4,12 +4,103 @@
 #include "rxode2parse.h"
 #include "rxode2parseHandleEvid.h"
 #include "rxode2parseGetTime.h"
+#define isSameTimeOp(xout, xp) (op->stiff == 0 ? isSameTimeDop(xout, xp) : isSameTime(xout, xp))
+#define max2( a , b )  ( (a) > (b) ? (a) : (b) )
 #if defined(__cplusplus)
+#include "timsort.h"
+#define SORT gfx::timsort
+static inline int handleExtraDose(int *neq,
+                                    int *BadDose,
+                                    double *InfusionRate,
+                                    double *dose,
+                                    double *yp,
+                                    double xout, double xp, int id,
+                                    int *i, int nx,
+                                    int *istate,
+                                    rx_solving_options *op,
+                                    rx_solving_options_ind *ind,
+                                    t_update_inis u_inis,
+                                    void *ctx) {
+    if (ind->extraDoseN[0] > ind->idxExtra) {
+      if (ind->extraSorted == 0) {
+        // do sort
+        SORT(ind->extraDoseTimeIdx + ind->idxExtra, ind->extraDoseTimeIdx + ind->extraDoseN[0],
+             [ind](int a, int b){
+               double timea = ind->extraDoseTime[a],
+                 timeb = ind->extraDoseTime[b];
+               if (timea == timeb) {
+                 int evida = ind->extraDoseEvid[a],
+                   evidb = ind->extraDoseEvid[b];
+                 if (evida == evidb){
+                   return a < b;
+                 }
+                 return evida < evidb;
+               }
+               return timea < timeb;
+             });
+        ind->extraSorted=1;
+        ind->idxExtra=0;
+      }
+      // Use "real" xout for handle_evid functions.
+      int idx = ind->idx;
+      int ixds = ind->ixds;
+      int trueIdx = ind->extraDoseTimeIdx[ind->idxExtra];
+      ind->idx = -1-trueIdx;
+      double time = getAllTimes(ind, ind->idx);
+      while (!isSameTimeOp(time, xp) && time < xp && ind->idxExtra < ind->extraDoseN[0]) {
+        ind->idxExtra++;
+        trueIdx = ind->extraDoseTimeIdx[ind->idxExtra];
+        ind->idx = -1-trueIdx;
+        time = getAllTimes(ind, ind->idx);
+      }
+      if ((isSameTimeOp(time, xp) || time > xp) && (isSameTimeOp(time, xout) || time <= xout)) {
+        bool ignore = true;
+        while (ignore && time <= xout) {
+          ignore=false;
+          for (int i = 0; i < ind->ignoredDosesN[0]; ++i) {
+            int curIdx = ind->ignoredDoses[i];
+            if (curIdx < 0 && -1-curIdx == trueIdx) {
+              ignore = true;
+              break;
+            }
+          }
+          if (ignore) {
+            ind->idxExtra++;
+            if (ind->idxExtra < ind->extraDoseN[0]) {
+              trueIdx = ind->extraDoseTimeIdx[ind->idxExtra];
+              ind->idx = -1-trueIdx;
+              time = getAllTimes(ind, ind->idx);
+            } else {
+              ind->idxExtra--;
+              break;
+            }
+          } else {
+            break;
+          }
+        }
+        if (ignore) {
+          ind->idx = idx;
+          ind->ixds = ixds;
+          return 0;
+        } else {
+          ind->extraDoseNewXout = time;
+          ind->idx = idx;
+          ind->ixds = ixds;
+          // REprintf("time: %f; xp: %f; xout: %f; handleExtra\n", time, xp, xout);
+          return 1;
+        }
+      }
+      ind->idx = idx;
+      ind->ixds = ixds;
+      return 0;
+    }
+    return 0;
+  }
+#undef SORT
+
 extern "C" {
 #endif
 
-#define isSameTimeOp(xout, xp) (op->stiff == 0 ? isSameTimeDop(xout, xp) : isSameTime(xout, xp))
-#define max2( a , b )  ( (a) > (b) ? (a) : (b) )
 #define badSolveExit(i) for (int j = op->neq*(ind->n_all_times); j--;){ \
     ind->solve[j] = NA_REAL;                                            \
   }                                                                     \
@@ -216,95 +307,6 @@ extern "C" {
       REprintf(" Rate is zero/negative\n");
     }
   }
-
-  static inline int handleExtraDose(int *neq,
-                                    int *BadDose,
-                                    double *InfusionRate,
-                                    double *dose,
-                                    double *yp,
-                                    double xout, double xp, int id,
-                                    int *i, int nx,
-                                    int *istate,
-                                    rx_solving_options *op,
-                                    rx_solving_options_ind *ind,
-                                    t_update_inis u_inis,
-                                    void *ctx) {
-    if (ind->extraDoseN[0] > ind->idxExtra) {
-      if (ind->extraSorted == 0) {
-        // do sort
-        SORT(ind->extraDoseTimeIdx + ind->idxExtra, ind->extraDoseTimeIdx + ind->extraDoseN[0],
-             [ind](int a, int b){
-               double timea = ind->extraDoseTime[a],
-                 timeb = ind->extraDoseTime[b];
-               if (timea == timeb) {
-                 int evida = ind->extraDoseEvid[a],
-                   evidb = ind->extraDoseEvid[b];
-                 if (evida == evidb){
-                   return a < b;
-                 }
-                 return evida < evidb;
-               }
-               return timea < timeb;
-             });
-        ind->extraSorted=1;
-        ind->idxExtra=0;
-      }
-      // Use "real" xout for handle_evid functions.
-      int idx = ind->idx;
-      int ixds = ind->ixds;
-      int trueIdx = ind->extraDoseTimeIdx[ind->idxExtra];
-      ind->idx = -1-trueIdx;
-      double time = getAllTimes(ind, ind->idx);
-      while (!isSameTimeOp(time, xp) && time < xp && ind->idxExtra < ind->extraDoseN[0]) {
-        ind->idxExtra++;
-        trueIdx = ind->extraDoseTimeIdx[ind->idxExtra];
-        ind->idx = -1-trueIdx;
-        time = getAllTimes(ind, ind->idx);
-      }
-      if ((isSameTimeOp(time, xp) || time > xp) && (isSameTimeOp(time, xout) || time <= xout)) {
-        bool ignore = true;
-        while (ignore && time <= xout) {
-          ignore=false;
-          for (int i = 0; i < ind->ignoredDosesN[0]; ++i) {
-            int curIdx = ind->ignoredDoses[i];
-            if (curIdx < 0 && -1-curIdx == trueIdx) {
-              ignore = true;
-              break;
-            }
-          }
-          if (ignore) {
-            ind->idxExtra++;
-            if (ind->idxExtra < ind->extraDoseN[0]) {
-              trueIdx = ind->extraDoseTimeIdx[ind->idxExtra];
-              ind->idx = -1-trueIdx;
-              time = getAllTimes(ind, ind->idx);
-            } else {
-              ind->idxExtra--;
-              break;
-            }
-          } else {
-            break;
-          }
-        }
-        if (ignore) {
-          ind->idx = idx;
-          ind->ixds = ixds;
-          return 0;
-        } else {
-          ind->extraDoseNewXout = time;
-          ind->idx = idx;
-          ind->ixds = ixds;
-          // REprintf("time: %f; xp: %f; xout: %f; handleExtra\n", time, xp, xout);
-          return 1;
-        }
-      }
-      ind->idx = idx;
-      ind->ixds = ixds;
-      return 0;
-    }
-    return 0;
-  }
-
 
   static inline void handleSSGen(int *neq,
                                  int *BadDose,
