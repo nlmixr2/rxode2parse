47e728245dca84bd489771f6f7192a10dc8a4c51
Matthew L. Fidler <matthew.fidler@gmail.com>
2023-11-04 11:44:42 -0500

    Don't cache udfs

--- R/rudf.R
+++ R/rudf.R
@@ -31,7 +31,9 @@
     x
   }, character(1), USE.NAMES = FALSE)
   if (.env$found) {
-    .ret <- c(.ret, data.table::address(.udfEnv$envir))
+    .ret <- c(.ret, data.table::address(.udfEnv$envir),
+              # don't cache md5 changes every run:
+              as.character(runif(1)))
   }
   .ret
 }
